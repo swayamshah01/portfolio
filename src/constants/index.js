84ef02db34dc4fb73812fed25dfa91132683f059
shahswayam01 <sswayam351@gmail.com>
2025-06-26 03:48:33 +0530

    Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,332 @@
+export const myProjects = [
+    {
+    id: 1,
+    title: "HackMate",
+    description:
+      "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
+    subDescription: [
+      "Developed a fully interactive Single Page Application (SPA) using Blazor WebAssembly.",
+      "Implemented API interactions using .NET Core for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated SQLite for efficient client-side database storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/HackMate.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "OAuth",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "Prisma",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "BookNest",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Python",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "CSS3",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "E-commerce Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/ecom.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/csharp.svg",
+      },
+      // {
+      //   id: 2,
+      //   name: ".Net",
+      //   path: "/assets/logos/dotnet.svg",
+      // },
+      // {
+      //   id: 3,
+      //   name: "Ef Core",
+      //   path: "/assets/logos/efcore.png",
+      // },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Learning Management System",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure SQLite database for user data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/LMS.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      // {
+      //   id: 2,
+      //   name: "React",
+      //   path: "/assets/logos/react.svg",
+      // },
+      // {
+      //   id: 3,
+      //   name: "SQLite",
+      //   path: "/assets/logos/sqlite.svg",
+      // },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+
+  
+  {
+    id: 5,
+    title: "Movie Ticket Booking System",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/Movie.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "MongoDB",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "Express",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 6,
+  //   title: "Online Learning Platform",
+  //   description:
+  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+  //   subDescription: [
+  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
+  //     "Implemented video streaming with Azure Media Services.",
+  //     "Added a quiz system with dynamic question generation and real-time grading.",
+  //     "Integrated Stripe API for secure payment processing.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/elearning.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Blazor",
+  //       path: "/assets/logos/blazor.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Azure",
+  //       path: "/assets/logos/azure.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Stripe",
+  //       path: "/assets/logos/stripe.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/swayam0108/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Lorem Ipsum Inc. - Software Engineer",
+    job: "lorem ipsum  Inc.",
+    date: "2021-2023",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  {
+    title: "Front-end Developer",
+    job: "Real Estate ",
+    date: "2025-present",
+    contents: [
+      "Develop responsive and visually appealing UIs using React.js and Tailwind CSS",
+      "Optimize components for speed and SEO performance ",
+      "Ensure cross-browser compatibility and mobile-first design",
+      "Collaborate with backend developers, designers, and product managers",
+      "🏠 Property Listing Grid & Filters – Built dynamic listing UI with search filters, price sliders, and sorting",
+      "📱 Mobile-First Dashboard – Developed a responsive user dashboard for managing listings and inquiries",
+      "📍 Interactive Map Integration – Integrated Mapbox for real-time property mapping and location filters",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
